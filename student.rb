ff0e6e30b54dc3a156599e5721a341605ed8ac28
PabloBona <pablobonasera@gmail.com>
2023-08-27 22:44:53 -0300

    Add class Teacher. Fix student

--- student.rb
+++ student.rb
@@ -1,8 +1,6 @@
 require_relative 'person'
 
-class Student
-  include Person
-
+class Student < Person
   def initialize(id, age, classroom, name = 'Unknown', parent_permission: true)
     super(id, age, name, parent_permission: parent_permission)
     @classroom = classroom
