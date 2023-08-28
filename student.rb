2109134284adce488970738e10fb5ed959edc869
PabloBona <pablobonasera@gmail.com>
2023-08-27 22:28:50 -0300

    Add class student

---
+++ student.rb
@@ -0,0 +1,14 @@
+require_relative 'person'
+
+class Student
+  include Person
+
+  def initialize(id, age, classroom, name = 'Unknown', parent_permission: true)
+    super(id, age, name, parent_permission: parent_permission)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    '¯\\(ツ)/¯'
+  end
+end
