25c33536953dcd47f7e4ff2a5ad914a35a958e47
Anwar Hussaini <anwarhussaini160@gmail.com>
2023-09-04 21:16:44 +0430

    Create create_person method on ui_interface.rb file

--- src/ui_interface.rb
+++ src/ui_interface.rb
@@ -25,7 +25,7 @@ class Interface
 
   def create_teacher
     print 'Age: '
-    age = gets.chomp
+    age = gets.chomp.to_i
 
     print 'Name: '
     name = gets.chomp
@@ -37,5 +37,23 @@ class Interface
     puts
 
     app.create_a_teacher(name, age, specialisation)
+  end
+
+  def create_person
+    loop do
+      print 'Would you like to create a student (1) or a teacher (2)? [Please enter a number]:'
+      option = gets.chomp.to_i
+
+      case option
+      when 1
+        create_student
+        break
+      when 2
+        create_teacher
+        break
+      else
+        puts 'Invalid number, Try again!'
+      end
+    end
   end
 end
