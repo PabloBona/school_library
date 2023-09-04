e90f5f7c935d80cd4e2c695ec87880f1445a67d3
Anwar Hussaini <anwarhussaini160@gmail.com>
2023-09-04 21:30:33 +0430

    Complete the helper functionalities for ui_interface.rb file

--- src/ui_interface.rb
+++ src/ui_interface.rb
@@ -7,6 +7,81 @@ class Interface
     @app = App.new
   end
 
+  def create_person
+    loop do
+      print 'Would you like to create a student (1) or a teacher (2)? [Please enter a number]:'
+      option = gets.chomp.to_i
+
+      case option
+      when 1
+        create_student
+        break
+      when 2
+        create_teacher
+        break
+      else
+        puts 'Invalid number, Try again!'
+      end
+    end
+  end
+
+  def create_book
+    print 'Title: '
+    title = gets.chomp
+
+    print 'Author: '
+    author = gets.chomp
+
+    app.create_a_book(title, author)
+    puts 'Book created successfully.'
+    puts
+  end
+
+  def create_a_rental
+    puts 'Choose a book from the list below by entering the corresponding number:'
+    list_books(with_id: true)
+    book = gets.chomp
+
+    puts 'Choose a person from the following list by number (not id)'
+    list_people(with_id: true)
+    person = gets.chomp
+
+    print 'Date: '
+    date = gets.chomp
+
+    puts 'Rental created successfully'
+    puts
+    app.create_a_rental(date, book.to_i, person.to_i)
+  end
+
+  def list_books(with_id: false)
+    if with_id
+      app.list_all_books.each.with_index(0) do |book, index|
+        puts "#{index}) Title: \"#{book.title}\", Author: #{book.author}"
+      end
+    else
+      app.list_all_books.each do |book|
+        puts "Title: \"#{book.title}\", Author: #{book.author}"
+      end
+    end
+    puts
+  end
+
+  def list_people(with_id: false)
+    if with_id
+      app.list_all_persons.each.with_index(0) do |person, index|
+        puts "#{index}) [#{person.class}] Name: \"#{person.name}\", ID: #{person.id}, Age: #{person.age}"
+      end
+    else
+      app.list_all_persons.each do |person|
+        puts "[#{person.class}] Name: \"#{person.name}\", ID: #{person.id}, Age: #{person.age}"
+      end
+    end
+    puts
+  end
+
+  private
+
   def create_student
     print 'Age: '
     age = gets.chomp
@@ -37,23 +112,5 @@ class Interface
     puts
 
     app.create_a_teacher(name, age, specialisation)
-  end
-
-  def create_person
-    loop do
-      print 'Would you like to create a student (1) or a teacher (2)? [Please enter a number]:'
-      option = gets.chomp.to_i
-
-      case option
-      when 1
-        create_student
-        break
-      when 2
-        create_teacher
-        break
-      else
-        puts 'Invalid number, Try again!'
-      end
-    end
   end
 end
