ad742fa2bd3cb72ea58b8a396240791a2c9d98c6
Anwar Hussaini <anwarhussaini160@gmail.com>
2023-09-06 21:38:06 +0430

    Fix a bug on student.rb file

--- src/student.rb
+++ src/student.rb
@@ -1,3 +1,4 @@
+require_relative 'person'
 class Student < Person
   def initialize(age, _classroom = nil, name = 'Unknown', parent_permission: true, id: nil)
     super(age, name, parent_permission: parent_permission, id: id)
