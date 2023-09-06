076bc75f91a1e30916bd91456b4fe1dc8a677e4f
dani <danifromecuador@gmail.com>
2023-09-06 17:29:49 -0500

    Fix rubocop linter errors

--- src/student.rb
+++ src/student.rb
@@ -1,6 +1,7 @@
 require_relative 'person'
 class Student < Person
   attr_accessor :classroom
+
   def initialize(age, classroom = nil, name = 'Unknown', parent_permission: true, id: nil)
     super(age, name, parent_permission: parent_permission, id: id)
     @classroom = classroom
